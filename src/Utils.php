8b75fc65ae8f5877c95d6e9183cd0ec3c70e0e4f
Neizzzy <azat.azatch@yandex.ru>
2025-04-14 13:15:10 +0300

    Add new funcs and fix stringify

--- src/Utils.php
+++ src/Utils.php
@@ -7,30 +7,81 @@ function convertToString(mixed $value): string
     return trim(var_export($value, true), "'");
 }
 
-function stringify(mixed $value, string $replacer = ' ', int $spacesCount = 1, int $currentDepth = 1)
+function getType(array $value): string
+{
+    return $value['type'];
+}
+
+function getKey(array $value): string
+{
+    return $value['key'];
+}
+
+function getValue(array $value): mixed
+{
+    return $value['value'];
+}
+
+function getChild(array $value): array
+{
+    return $value['child'];
+}
+
+function makeIndent(int $depth = 1, int $shift = 0, int $spacesCount = 4): string
+{
+    return str_repeat(' ', $spacesCount * $depth - $shift);
+}
+
+function formatDiff(array $tree, int $currentDepth = 1)
+{
+    $result = array_map(function ($node) use ($currentDepth) {
+        $type = getType($node);
+        $key = getKey($node);
+        $indent = makeIndent($currentDepth);
+        $reducedIndent = makeIndent($currentDepth, 2);
+
+        return match ($type) {
+            'nested' =>
+                "{$indent}{$key}: {\n" . formatDiff(getChild($node), $currentDepth + 1) . "\n{$indent}}",
+
+            'unchanged' =>
+                "{$indent}{$key}: " . stringify(getValue($node), $currentDepth),
+
+            'updated' =>
+                "{$reducedIndent}- {$key}: " . stringify(getValue($node)['firstValue'], $currentDepth) . "\n" .
+                "{$reducedIndent}+ {$key}: " . stringify(getValue($node)['secondValue'], $currentDepth),
+
+            'removed' =>
+                "{$reducedIndent}- {$key}: " . stringify(getValue($node), $currentDepth),
+
+            'added' =>
+                "{$reducedIndent}+ {$key}: " . stringify(getValue($node), $currentDepth),
+        };
+    }, $tree);
+
+    return implode("\n", $result);
+}
+
+
+function stringify(mixed $value, int $currentDepth): string
 {
     if (!is_array($value)) {
         return convertToString($value);
     }
 
-    $indent = str_repeat($replacer, ($currentDepth - 1) * $spacesCount);
-    $innerIndent = str_repeat($replacer, $currentDepth * $spacesCount);
-
     $keys = array_keys($value);
+    $result = array_map(function ($key) use ($value, $currentDepth) {
+        $newDepth = $currentDepth + 1;
+        $indent = makeIndent($newDepth);
+        return "{$indent}{$key}: " . stringify($value[$key], $newDepth);
+    }, $keys);
 
-    $fn = function ($acc, $key) use ($replacer, $spacesCount, $currentDepth, $innerIndent, $value) {
-        if (is_array($value[$key])) {
-            $acc[] = $innerIndent . "$key: " . stringify($value[$key], $replacer, $spacesCount, $currentDepth + 1);
-        } else {
-            $acc[] = $innerIndent . "$key: " . convertToString($value[$key]);
-        }
-        return $acc;
-    };
-    $result = array_reduce($keys, $fn, []);
+    $indentEndBrace = makeIndent($currentDepth);
+    return "{\n" . implode("\n", $result) . "\n{$indentEndBrace}}";
+}
 
-    if (empty($result)) {
-        return "{}";
-    }
-
-    return "{\n" . implode("\n", $result) . "\n" . $indent . "}";
+function formatStylish(array $diff): string
+{
+    $result = formatDiff($diff);
+    return "{\n{$result}\n}";
 }
