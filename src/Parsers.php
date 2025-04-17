727437f54d62509cb7d9b2cbe030ac5fd06af085
Neizzzy <azat.azatch@yandex.ru>
2025-04-17 15:10:02 +0300

    Add new functions

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,22 +7,28 @@ use Symfony\Component\Yaml\Yaml;
 
 const EXCEPTED_EXTENSIONS = ['json', 'yml', 'yaml'];
 
-function parseJson(string $path): array
+function getFileContent(string $path): string
 {
     $content = file_get_contents($path);
     if ($content === false) {
         throw new Exception("Parse Error: Failed to read {$path}");
     }
 
+    return $content;
+}
+
+function getFileExtension(string $path): string
+{
+    return pathinfo($path, PATHINFO_EXTENSION);
+}
+
+function parseJson(string $content): array
+{
     return json_decode($content, true, flags: JSON_OBJECT_AS_ARRAY);
 }
 
-function parseYaml(string $path): array
+function parseYaml(string $content): array
 {
-    $content = file_get_contents($path);
-    if ($content === false) {
-        throw new Exception("Parse Error: Failed to read {$path}");
-    }
     return Yaml::parse($content);
 }
 
@@ -32,12 +38,14 @@ function parseFile(string $path): array
         throw new Exception("Path Error: File {$path} not found!\n");
     }
 
-    $fileExtension = pathinfo($path, PATHINFO_EXTENSION);
+    $content = getFileContent($path);
+
+    $fileExtension = getFileExtension($path);
     $extensionsString = implode(', ', EXCEPTED_EXTENSIONS);
 
     $parsed = match (true) {
-        $fileExtension === 'json' => parseJson($path),
-        $fileExtension === 'yaml' || $fileExtension === 'yml' => parseYaml($path),
+        $fileExtension === 'json' => parseJson($content),
+        $fileExtension === 'yaml' || $fileExtension === 'yml' => parseYaml($content),
         default => throw new Exception("Extension Error: File {$path} must be {$extensionsString}\n"),
     };
 
